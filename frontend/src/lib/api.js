3f071bfa7fffb6ba4f1b789fd68e394f2b1b7575
sunnykumar37 <sunnyk28912@gmail.com>
2025-09-26 13:57:18 +0530

    new url be

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://edu-connect-sih.onrender.com/',
+  baseURL: 'https://edu-connect-sih-1.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
